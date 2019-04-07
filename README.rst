67b263cec4bbac2313533d56214400dcd25281b0
Bharat Sri Harsha Karpurapu <kbsriharsha@Bharats-MBP.fios-router.home>
2019-04-07 18:38:26 -0400

    Setup.py

---
+++ README.rst
@@ -0,0 +1,20 @@
+===============================
+Zeppi_Convert
+===============================
+
+.. image:: https://img.shields.io/travis/kbsriharsha/Zeppi_Convert.svg
+        :target: https://travis-ci.org/kbsriharsha/Zeppi_Convert
+
+.. image:: https://img.shields.io/pypi/v/Zeppi_Convert.svg
+        :target: https://pypi.python.org/pypi/Zeppi_Convert
+
+
+Convert your zeppelin notebooks to python
+
+* Free software: MIT license
+* Documentation: (COMING SOON!) https://Zeppi_Convert.readthedocs.org.
+
+Features
+--------
+
+* TODO
--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as readme_file:
+    readme = readme_file.read()
 
 setup(
     name = 'Zeppi_Convert',
     version = '0.1.0',
     description="Convert your zeppelin notebooks to python",
+    long_description=readme
     author="Harsha Karpurapu",
     author_email='kbsriharsha@gmail.com',
     packages = find_packages(exclude=['contrib', 'docs', 'tests']),
