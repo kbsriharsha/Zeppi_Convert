2b93385d6ab6bbbb4fa77fc63ad6987272cd5af9
Bharat Sri Harsha Karpurapu <kbsriharsha@Bharats-MBP.fios-router.home>
2019-04-07 18:56:00 -0400

    adding pypirc

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as readme_file:
 
 setup(
     name = 'Zeppi_Convert',
-    version = '0.1.0',
+    version = '0.1.1',
     description="Convert your zeppelin notebooks to python",
     long_description=readme,
     author = "Harsha Karpurapu",
